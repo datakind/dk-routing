5e08ef73f3f209267b2d6b45175bf170b0ff7185
Zebreu <sebouel@gmail.com>
2024-03-29 23:47:19 -0400

    Add server parameters (maybe not required) and fix up some aesthetics things

--- dkroutingtool/src/py/ui/dashboard.py
+++ dkroutingtool/src/py/ui/dashboard.py
@@ -92,11 +92,10 @@ def main():
         lats = [coords[0][i][0] for i in range(5)] # 5 because we expect a rectangle including its center point
         lons = [coords[0][i][1] for i in range(5)]
         bounding_box = [min(lats), min(lons), max(lats), max(lons)]
-        st.write(f"Bounding box: {bounding_box}")
         area = abs(bounding_box[2] - bounding_box[0]) * abs(bounding_box[3] - bounding_box[1]) 
-        st.write(f"Bounding box: {bounding_box}, area: {area} square units")
-        if area > 0.01:
-            st.write(f'Please choose a smaller area. We allow areas below 0.01')
+        st.write(f"Bounding box: {bounding_box}, area: {round(area,5)} square units")
+        if area > 0.03:
+            st.write(f'Please choose a smaller area. We allow areas below 0.03')
         else:
             map_requested = st.button('Click here to download the area. You do not need to download it again if you try out multiple solutions below')
             if map_requested:
