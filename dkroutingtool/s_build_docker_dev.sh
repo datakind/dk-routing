4133bed86e53d0a8417aaea91f926c85d8ca216d
Sébastien Ouellet <sebouel@gmail.com>
2024-03-31 21:14:00 -0400

    Update s_build_docker_dev.sh

--- dkroutingtool/s_build_docker_dev.sh
+++ dkroutingtool/s_build_docker_dev.sh
@@ -4,5 +4,5 @@ FILEPATH=$(yq e '.Build."osm-data"."geofabrik-url"' build_parameters.yml)
 FILENAME=$(basename $FILEPATH)
 SHORTFILENAME=${FILENAME%%.*} #removes extensions
 
-docker build --platform=linux/amd64 --memory=8g --build-arg osm_download_url=$FILEPATH --build-arg osm_filename_arg=$SHORTFILENAME -f Dockerfile.dev -t dkroutingtool:dev .
+docker build --platform=linux/amd64 --memory=8g --build-arg osm_download_url=$FILEPATH --build-arg osm_filename_arg=$SHORTFILENAME -f Dockerfile.dev -t dk-routing:main .
 
